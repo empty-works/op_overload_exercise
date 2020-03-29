8824cd850765b395ac07d028dce9319df752e10e
empty-works <pama.mark@gmail.com>
2020-03-29 23:21:55 +0200

    Added the function prototypes in the header file.

--- MyString.h
+++ MyString.h
@@ -5,5 +5,13 @@ class MyString {
 private:
 	char *str {};
 public:
-
+	MyString operator-() const;
+	bool operator==(const MyString &rhs) const;
+	bool operator!=(const MyString &rhs) const;
+	bool operator<(const MyString &rhs) const;
+	bool operator>(const MyString &rhs) const;
+	MyString operator+(const MyString &rhs) const;
+	MyString operator+=(const MyString &rhs) const;
+	MyString operator*(const MyString &rhs) const;
+	MyString operator*=(const MyString &rhs) const;
 };
